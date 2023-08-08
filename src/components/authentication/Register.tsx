6684daac1cb2db8712d1c0b176fd467e5879f7da
Marie20767 <marie20767@gmail.com>
2023-08-08 18:10:29 +0300

    Fixed 401 registration bug

--- src/components/authentication/Register.tsx
+++ src/components/authentication/Register.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import { useAuth } from "src/contexts/AuthContext";
 import { FormSubmitEvent } from "src/types";
@@ -18,23 +17,11 @@ const Register = () => {
   const [isPasswordShowing, setIsPasswordShowing] = useState({ password: false, repeatPassword: false });
   const [error, setError] = useState("");
 
-  const { signup, updateUser, currentUser } = useAuth();
-
-  const navigate = useNavigate();
+  const { signup, updateUser } = useAuth();
 
   const addUserToDB = useRequest("/register");
 
   const formGroupClassname = "mb-2 pe-4";
-
-  // TODO: this will be handled in UnauthenticatedRoute component instead
-  // This logic currently handles redirecting the user to home page if they are logged in
-  // Maybe what we could do instead is handle the redirection before even rendering this component
-  // useEffect(() => {
-  //   // Make sure we only navigate to register when we have a currentUser
-  //   if (currentUser) {
-  //     navigate("/");
-  //   }
-  // }, [currentUser]);
 
   const onError = (errorName: string, error: string) => {
     console.log(`${errorName}: ${error}`);
@@ -47,7 +34,7 @@ const Register = () => {
     setError("");
 
     if (repeatPasswordInput !== passwordInput) {
-      setError("Please make sure your passwords match");
+      setError("Please make sure your passwords match.");
     } else {
       try {
         setIsLoading(true);
@@ -56,14 +43,17 @@ const Register = () => {
 
         await updateUser(newUser, nameInput);
 
+        const accessToken = await newUser.user.getIdToken();
+
         try {
           addUserToDB({
             requestBody: {
               name: nameInput,
               email: emailInput,
-              id: currentUser?.uid,
+              id: newUser?.user?.uid,
+              access_token: accessToken,
             },
-            onSuccess: () => navigate("/"),
+            onSuccess: () => {},
             onError: (error) => onError("addUserToDBError", error)
           });
         } catch (addNewUserToDBError) {
