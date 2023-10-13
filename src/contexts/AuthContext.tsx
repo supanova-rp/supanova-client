96a605036a7dfef26878d6e9a2695c4d61472b43
Marie20767 <marie20767@gmail.com>
2023-10-13 14:15:45 +0100

    Removed unnecessary console log for currentUser

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -41,8 +41,6 @@ export const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) =>
   const [currentUser, setCurrentUser] = useState<FirebaseUser | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  console.log(">>> currentUser: ", currentUser);
-
   const navigate = useNavigate();
   const location = useLocation().pathname;
 
