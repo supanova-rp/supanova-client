b94cedb367bd40bd16e39cd3a8be9b767266bb0d
Marie20767 <marie20767@gmail.com>
2023-10-23 11:49:47 +0100

    Changed env vars in firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 
 const app = initializeApp({
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
