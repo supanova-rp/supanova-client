30bd07a92d0a34a173bbc6951871f37501c91f59
Jamie Garner <jamiegarner123@gmail.com>
2026-02-22 17:54:13 +1100

    add temp message

--- src/components/admin/progress/ProgressDashboard.tsx
+++ src/components/admin/progress/ProgressDashboard.tsx
@@ -11,6 +11,7 @@ import AdminHeader from "../AdminHeader";
 
 const ProgressDashboard = () => {
   const [expandedUsers, setExpandedUsers] = useState<Set<ID>>(new Set());
+  const [isHappyBirthdayExpanded, setIsHappyBirthdayExpanded] = useState(false);
   const { data, loading, error, refetch } = useQuery<ProgressAdminView[]>(
     "/admin/get-all-progress",
     {
@@ -58,6 +59,44 @@ const ProgressDashboard = () => {
         warningMessage="No user progress data available."
       >
         <div className="progress-dashboard">
+          <div className="progress-user-card">
+            <ExpandCollapseButton
+              isExpanded={isHappyBirthdayExpanded}
+              className="progress-user-header"
+              onClick={() => setIsHappyBirthdayExpanded(prev => !prev)}
+            >
+              <div className="progress-user-info">
+                <h5 className="progress-user-name">Happy Birthday Joel!</h5>
+              </div>
+            </ExpandCollapseButton>
+            {isHappyBirthdayExpanded ? (
+              <div
+                className="progress-user-breakdown"
+                style={{ paddingTop: 12 }}
+              >
+                <p>
+                  This dashboard tracks how far users have gotten in each course
+                  (green dot completed section, grey dot not completed section),
+                  whether they&apos;ve completed the intro or entire course.
+                </p>
+                <p>
+                  It also shows any previous attempts they had at the quizzes
+                  (showing which questions they answered incorrectly) and if it
+                  hasn&apos;t been completed yet it will also show what
+                  they&apos;ve selected so far in their current attempt.
+                </p>
+                <p>
+                  NOTE - one caveat with the quiz attempt history, the backend
+                  wasn&apos;t collecting this data before so this will only work
+                  for future quiz attempts/submissions as there is no data for
+                  anything before this feature was added :(. Anyway, hope you
+                  like it, let me know if any bugs :3.
+                </p>
+                <p>From Jambo!</p>
+              </div>
+            ) : null}
+          </div>
+
           {data?.map(userProgress => {
             const isExpanded = expandedUsers.has(userProgress.userID);
             return (
